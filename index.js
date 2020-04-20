7668eef7496ec41e0046d0a44edc91b4e33e78eb
Rajkiran Gurunathan (TA-14) <Rajkiran.GA.Gurunathan@bmw.de>
2020-04-20 23:07:11 +0200

    change pool connection method

--- index.js
+++ index.js
@@ -1,85 +1,112 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const path = require('path')
-const mysql = require('mysql')
+require("dotenv").config();
+var express = require("express");
+var mysql = require("mysql");
+const cors = require("cors");
+var app = express();
 
-// Create the server
-const app = express()
-// Serve static files from the React frontend app
-app.use(express.static(path.join(__dirname, 'client/build')))
 app.use(cors());
 
-const SELECT_ALL_USERS = 'select * from users'
-const SELECT_ALL_PRODUCTS = 'select * from products'
-const SELECT_DETAIL_PRODUCT = 'select * from detailProducts'
-
-const connection = mysql.createConnection({
+var connection = mysql.createPool({
+  connectionLimit: 50,
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
-  database: process.env.DB_name
-})
+  database: process.env.DB_name,
+});
 
-connection.connect(err => {
-  if(err) {
-    return err;
-  }setInterval(function () {
-    connection.query('SELECT 1');
-}, 5000);
-})
-console.log(connection);
-
-app.get('/', (req, res) => {
-  res.send('go to the home page')
-})
-
-
-app.get('/products', (req, res) => {
-  connection.query(SELECT_ALL_PRODUCTS, (err, results) => {
-    if(err) {
-      return res.send(err)
+app.get("/products", function (req, res) {
+  connection.getConnection(function (error, tempCont) {
+    if (!!error) {
+      tempCont.release();
+      console.log("Error");
+    } else {
+      tempCont.query("select * from products", function (error, rows, fields) {
+        tempCont.release();
+        if (!!error) {
+          console.log("Error in the products query");
+        } else {
+          return res.json({
+            data: rows,
+          });
+        }
+      });
     }
-    else {
-      return res.json({
-        data: results
-      })
-    }
-  })
-})
+  });
+});
 
-app.get('/detailProduct', (req, res) => {
-  connection.query(SELECT_DETAIL_PRODUCT, (err, results) => {
-    if(err) {
-      return res.send(err)
+app.get("/detailProduct", function (req, res) {
+  connection.getConnection(function (error, tempCont) {
+    if (!!error) {
+      tempCont.release();
+      console.log("Error");
+    } else {
+      tempCont.query("select * from detailProducts", function (
+        error,
+        rows,
+        fields
+      ) {
+        tempCont.release();
+        if (!!error) {
+          console.log("Error in the detailProducts query");
+        } else {
+          return res.json({
+            data: rows,
+          });
+        }
+      });
     }
-    else {
-      return res.json({
-        data: results
-      })
-    }
-  })
-})
+  });
+});
 
-app.get('/userDetails/add', (req, res) => {
-  const { uid, name, email, phone, address, euro, dollar } = req.query;
-  const INSERT_USERS = `INSERT INTO userDetails (uid, name, email, phone, address, euro, dollar) values('${uid}','${name}','${email}','${phone}','${address}','${euro}','${dollar}')`;
-  connection.query(INSERT_USERS, (err, results) => {
-    if(err) {
-      return res.send(err)
+app.get("/userDetails", function (req, res) {
+  connection.getConnection(function (error, tempCont) {
+    if (!!error) {
+      tempCont.release();
+      console.log("Error");
+    } else {
+      tempCont.query("select * from userDetails", function (
+        error,
+        rows,
+        fields
+      ) {
+        tempCont.release();
+        if (!!error) {
+          console.log("Error in the userDetails query");
+        } else {
+          return res.json({
+            data: rows,
+          });
+        }
+      });
     }
-    else {
-      return res.send('successfully added users')
-    }
-  })
-})
-// Anything that doesn't match the above, send back the index.html file
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname + '/client/build/index.html'))
-})
+  });
+});
 
-// Choose the port and start the server
-const PORT = process.env.PORT || 5000
+app.get("/userDetails/add", function (req, res) {
+  const { uid, name, address, euro, dollar } = req.query;
+  connection.getConnection(function (error, tempCont) {
+    if (!!error) {
+      tempCont.release();
+      console.log("Error");
+    } else {
+      tempCont.query(
+        `INSERT INTO userDetails (uid, name, address, euro, dollar) values('${uid}','${name}','${address}','${euro}','${dollar}')`,
+        function (error, rows, fields) {
+          tempCont.release();
+          if (!!error) {
+            console.log("Error in the userDetails/add query");
+          } else {
+            return res.json({
+              data: rows,
+            });
+          }
+        }
+      );
+    }
+  });
+});
+
+const PORT = process.env.PORT || 1337;
 app.listen(PORT, () => {
-  console.log(`Mixing it up on port ${PORT}`)
-})
+  console.log(`Mixing it up on port ${PORT}`);
+});
